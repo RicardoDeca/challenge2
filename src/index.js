279a5c621f35efb3cf49a8b818914b21be553d38
RicardoDeca <104797871+RicardoDeca@users.noreply.github.com>
2022-06-11 14:17:09 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+let age = 19;
+if (age > 18) {
+  console.log("Voce pode passar");
+}
+let human = true;
+if (age > 18 && human == true) {
+  console.log("As condições são reais");
+}
+
+let jan = true;
+let dez = false;
+if (jan == true || dez == true) {
+  console.log("parabéns é o mês do seu aniversário");
+}
